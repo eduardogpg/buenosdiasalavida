d237213d1780b6c4c43664aeadac71dc92c534e6
eduardogpg <eduardo78d@gmail.com>
2022-07-23 18:53:54 -0500

    fa

--- buenosdias/settings.py
+++ buenosdias/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
--- buenosdias/urls.py
+++ buenosdias/urls.py
@@ -18,7 +18,10 @@ from django.urls import path
 
 from .views import index
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', index)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
